f1f5ae1b21dc14eeec3cfe0ce0539ca2b165fd28
Rubén Sospedra <rsospedra@cameo.com>
2021-08-04 16:21:17 +0200

    feat: Add counter

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class TOSDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <script
+            data-goatcounter="https://olympics.goatcounter.com/count"
+            async
+            src="//gc.zgo.at/count.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
